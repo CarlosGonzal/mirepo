20259ffc077d14c3b73a7d250558ce5176bc9d62
Carlos <cgonzalezdelpozo@gmail.com>
2022-11-01 19:17:02 +0100

    Modulo4

---
+++ Modulo4/app.js
@@ -0,0 +1,93 @@
+//Laboratorio 4
+
+// Sum numbers with functions
+
+// function sumNumbers (numA , numB) {
+//     var resultSum = numA + numB ;
+//     console.log("The result of the sum is: " + resultSum);
+// }
+
+// sumNumbers(11, 4);
+
+// // substraction numbers with function.
+
+// function subNumbers (numA , numB) {
+//     var resultSub = numA - numB ;
+//     console.log("The result of the substraction is: " + resultSub);
+// }
+
+// subNumbers(6, 8);
+
+// // Multiplication number with function.
+
+// function mulNumbers (numA, numB) {
+//     var resultMul = numA * numB;
+//     console.log("The result of the multiplication is: " + resultMul);
+// }
+
+// mulNumbers(3,3);
+
+// // Division numbers with function
+
+// function divNumbers (numA, numB) {
+//     var resultDiv = numA / numB;
+//     console.log("The result of the division is:" , resultDiv);
+// }
+
+// divNumbers(4,2)
+
+// console.log(sumNumbers());
+// console.log(subNumbers());
+// console.log(mulNumbers());
+// console.log(divNumbers());
+
+
+// Inputs
+
+   var numA = () => parseInt(document.getElementById("first-number").value);
+   var numB = () => parseInt(document.getElementById("second-number").value);
+
+// Sum numbers with functions
+
+   var sumNumbers = () => numA() + numB();
+
+// substraction numbers with function.
+
+   var subNumbers = () => numA() - numB();
+
+
+// Multiplication number with function.
+
+   var mulNumbers = () => numA() * numB();
+
+
+// Division numbers with function
+
+   var divNumbers = () => numA() / numB();
+
+
+// Result
+
+   var ResultSum = () => document.getElementById("result").innerText = sumNumbers();
+   var ResultSub = () => document.getElementById("result").innerText = subNumbers();
+   var ResultMul = () => document.getElementById("result").innerText = mulNumbers();
+   var ResultDiv = () => document.getElementById("result").innerText = divNumbers();
+
+
+// Events
+
+   document.getElementById("sum").addEventListener("click", ResultSum);
+   document.getElementById("sub").addEventListener("click", ResultSub);
+   document.getElementById("mul").addEventListener("click", ResultMul);
+   document.getElementById("div").addEventListener("click", ResultDiv);
+
+//Null Field
+
+   if (!numA() || !numB()){
+         document.getElementById("result").innerText = "You should use 2 numbers";
+      }
+
+
+
+
+
